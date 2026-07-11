46754801a6bacb28cbceb9cec6c35b73cec4fb74
Saurabh Yadav <saurabhyadav9569@gmail.com>
2026-07-11 08:29:02 +0530

    Update API URL and JWT middleware

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://trello-lite-1-yzzt.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Attach JWT token to every request
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
